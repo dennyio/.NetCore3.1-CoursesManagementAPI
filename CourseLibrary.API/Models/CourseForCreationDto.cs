3a1d0eefef5b050b9d678d5aee9cb192c3378f6b
Denny Capellan <denny.capellan@outlook.com>
2020-01-02 21:55:15 -0500

    Adding data validation

--- CourseLibrary.API/Models/CourseForCreationDto.cs
+++ CourseLibrary.API/Models/CourseForCreationDto.cs
@@ -1,13 +1,30 @@
-﻿using System;
+﻿using CourseLibrary.API.ValidationAttributes;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace CourseLibrary.API.Models
 {
-    public class CourseForCreationDto
+    [CourseTitleMustBeDifferentFromDescription(
+        ErrorMessage ="Title must be different from description")]
+    public class CourseForCreationDto //: IValidatableObject
     {
+        [Required(ErrorMessage ="You should fill out a title.")]
+        [MaxLength(100)]
         public string Title { get; set; }
-        public string Description { get; set; }       
+        [MaxLength(1500)]
+        public string Description { get; set; }
+
+        //public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        //{
+        //   if (Title == Description)
+        //    {
+        //        yield return new ValidationResult(
+        //            "The provided description should be different from the title.",
+        //            new[] { "CourseForCreationDto" });
+        //    }
+        //}
     }
 }
