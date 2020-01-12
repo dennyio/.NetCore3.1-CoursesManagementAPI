425ed5300bcdedffdce4a3b8b02eed9325294bcb
Denny Capellan <denny.capellan@outlook.com>
2020-01-11 19:01:25 -0500

    Property mapping

--- CourseLibrary.API/Services/PropertyMappingService.cs
+++ CourseLibrary.API/Services/PropertyMappingService.cs
@@ -7,6 +7,13 @@ namespace CourseLibrary.API.Services
 {
     public class PropertyMappingService
     {
-
+        private Dictionary<string, PropertyMappingValue> _authorPropertyMapping =
+          new Dictionary<string, PropertyMappingValue>(StringComparer.OrdinalIgnoreCase)
+          {
+               { "Id", new PropertyMappingValue(new List<string>() { "Id" } ) },
+               { "MainCategory", new PropertyMappingValue(new List<string>() { "MainCategory" } )},
+               { "Age", new PropertyMappingValue(new List<string>() { "DateOfBirth" } , true) },
+               { "Name", new PropertyMappingValue(new List<string>() { "FirstName", "LastName" }) }
+          };
     }
 }
